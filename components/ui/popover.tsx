2386253258402d205c11039a6d1beadf0a1a25e9
Your Name <yayafaresPRO@gmail.com>
2025-12-23 22:27:49 +0100

    fix: Increase popover z-index for fullscreen modal compatibility

--- components/ui/popover.tsx
+++ components/ui/popover.tsx
@@ -21,7 +21,7 @@ const PopoverContent = React.forwardRef<
       align={align}
       sideOffset={sideOffset}
       className={cn(
-        "z-50 w-72 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none",
+        "z-[10000] w-72 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none",
         className
       )}
       {...props}
