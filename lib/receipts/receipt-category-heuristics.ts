5f870f8b980df22a84e6f5d8f82e4f17ed7730d3
Your Name <yayafaresPRO@gmail.com>
2025-12-29 02:49:05 +0100

    Fixes

--- lib/receipts/receipt-category-heuristics.ts
+++ lib/receipts/receipt-category-heuristics.ts
@@ -25,19 +25,22 @@ export function suggestReceiptCategoryNameFromDescription(params: {
   }
 
   // Carbs / staples
-  if (/\b(rice|arroz)\b/.test(normalized)) {
-    return pick("Pasta, Rice & Cereal", "Bread & Bakery", "Other")
+  if (/\b(rice|arroz|potato|patata)\b/.test(normalized)) {
+    return pick("Pasta, Rice & Grains", "Bread", "Other")
   }
   if (/\b(pasta|spaghetti|noodle|noodles|macaroni|ramen)\b/.test(normalized)) {
-    return pick("Pasta, Rice & Cereal", "Other")
+    return pick("Pasta, Rice & Grains", "Other")
   }
   if (/\b(bread|baguette|bagel|tortilla|wrap|bun)\b/.test(normalized)) {
-    return pick("Bread & Bakery", "Other")
+    return pick("Bread", "Wraps & Buns", "Other")
   }
 
   // Protein
-  if (/\b(chicken|pollo|beef|ternera|pork|cerdo|sausage|salchicha|ham|jamon|turkey|pavo|bacon)\b/.test(normalized)) {
-    return pick("Meat", "Deli", "Other")
+  if (/\b(chicken|pollo|beef|ternera|pork|cerdo|turkey|pavo)\b/.test(normalized)) {
+    return pick("Meat & Poultry", "Other")
+  }
+  if (/\b(sausage|salchicha|ham|jamon|bacon|salami|chorizo|cold cut|deli meat)\b/.test(normalized)) {
+    return pick("Deli / Cold Cuts", "Meat & Poultry", "Other")
   }
   if (/\b(salmon|salm[oó]n|tuna|atun|shrimp|prawn|fish|pescado|seafood|marisco)\b/.test(normalized)) {
     return pick("Fish & Seafood", "Other")
@@ -47,44 +50,71 @@ export function suggestReceiptCategoryNameFromDescription(params: {
   }
 
   // Dairy
-  if (/\b(milk|leche|yogurt|yoghurt|yogur|cheese|queso|butter|mantequilla)\b/.test(normalized)) {
-    return pick("Dairy", "Deli", "Other")
+  if (/\b(cheese|queso)\b/.test(normalized)) {
+    return pick("Cheese", "Other")
+  }
+  if (/\b(milk|leche|yogurt|yoghurt|yogur)\b/.test(normalized)) {
+    return pick("Dairy (Milk/Yogurt)", "Other")
+  }
+  if (/\b(butter|mantequilla)\b/.test(normalized)) {
+    return pick("Oils & Vinegars", "Condiments", "Other")
   }
 
   // Fruits & vegetables
   if (/\b(apple|manzana|banana|platano|orange|naranja|grape|uva|strawberry|fresa|avocado|aguacate|peach|melocoton)\b/.test(normalized)) {
     return pick("Fruits", "Other")
   }
-  if (/\b(potato|patata|onion|cebolla|carrot|zanahoria|broccoli|brocoli|pepper|pimiento|tomato|tomate|cucumber|pepino|lettuce|lechuga|salad|ensalada)\b/.test(normalized)) {
+  if (/\b(onion|cebolla|carrot|zanahoria|broccoli|brocoli|pepper|pimiento|tomato|tomate|cucumber|pepino|lettuce|lechuga)\b/.test(normalized)) {
     return pick("Vegetables", "Other")
+  }
+  if (/\b(salad|ensalada)\b/.test(normalized) && /\b(prepared|ready|fresh)\b/.test(normalized)) {
+    return pick("Prepared Salads", "Fresh Ready-to-Eat", "Other")
   }
 
   // Drinks
-  if (/\b(water|agua)\b/.test(normalized)) {
-    return pick("Water", "Drinks", "Beverages", "Other")
+  if (/\b(water|agua)\b/.test(normalized) && !/\b(coconut|coco)\b/.test(normalized)) {
+    return pick("Water", "Other")
   }
-  if (/\b(energy)\b/.test(normalized)) {
-    return pick("Energy Drinks", "Drinks", "Beverages", "Other")
+  if (/\b(energy|energetica)\b/.test(normalized) && /\b(drink|bebida)\b/.test(normalized)) {
+    return pick("Energy & Sports Drinks", "Soft Drinks", "Other")
   }
-  if (/\b(cola|coke|coca|soda|refresco)\b/.test(normalized)) {
-    return pick("Soda & Cola", "Drinks", "Beverages", "Other")
+  if (/\b(cola|coke|coca|soda|refresco|sprite|fanta|pepsi)\b/.test(normalized)) {
+    return pick("Soft Drinks", "Other")
   }
   if (/\b(juice|zumo)\b/.test(normalized)) {
-    return pick("Juice", "Drinks", "Beverages", "Other")
+    return pick("Juice", "Other")
   }
   if (/\b(coffee|cafe|tea|te)\b/.test(normalized)) {
-    return pick("Coffee & Tea", "Drinks", "Beverages", "Other")
+    return pick("Coffee & Tea", "Other")
   }
-  if (/\b(beer|cerveza|wine|vino|alcohol)\b/.test(normalized)) {
-    return pick("Alcohol", "Drinks", "Beverages", "Other")
+  if (/\b(beer|cerveza)\b/.test(normalized)) {
+    return pick("Beer", "Other")
+  }
+  if (/\b(wine|vino)\b/.test(normalized)) {
+    return pick("Wine", "Other")
+  }
+  if (/\b(vodka|whisky|whiskey|rum|ron|gin|ginebra|tequila|spirits?)\b/.test(normalized)) {
+    return pick("Spirits", "Other")
   }
 
   // Household / personal care
-  if (/\b(detergent|lavadora|lejia|bleach|cleaner|limpiador|dish|lavavajillas|soap|jabon|paper towel|servilleta)\b/.test(normalized)) {
-    return pick("Household & Cleaning Supplies", "Other")
+  if (/\b(detergent|lavadora|lejia|bleach|cleaner|limpiador|dish|lavavajillas)\b/.test(normalized)) {
+    return pick("Cleaning Supplies", "Laundry", "Other")
   }
-  if (/\b(shampoo|conditioner|deodorant|toothpaste|pasta de dientes|floss|hilo dental)\b/.test(normalized)) {
-    return pick("Personal Care", "Other")
+  if (/\b(soap|jabon)\b/.test(normalized) && !/\b(dish|lavavajillas)\b/.test(normalized)) {
+    return pick("Hygiene & Toiletries", "Other")
+  }
+  if (/\b(paper towel|servilleta|tissue|papel)\b/.test(normalized)) {
+    return pick("Paper Goods", "Other")
+  }
+  if (/\b(shampoo|conditioner)\b/.test(normalized)) {
+    return pick("Hair Care", "Other")
+  }
+  if (/\b(deodorant|desodorante)\b/.test(normalized)) {
+    return pick("Hygiene & Toiletries", "Other")
+  }
+  if (/\b(toothpaste|pasta de dientes|floss|hilo dental|toothbrush|cepillo)\b/.test(normalized)) {
+    return pick("Oral Care", "Other")
   }
 
   return null
