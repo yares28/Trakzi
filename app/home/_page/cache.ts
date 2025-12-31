a1e02f87770f38a5346e0fccf9305fb55ced3c19
Your Name <yayafaresPRO@gmail.com>
2025-12-31 20:03:48 +0100

    Parsing v2 phase 1 and 2 and v1 deleted

---
+++ app/home/_page/cache.ts
@@ -0,0 +1,7 @@
+// Simple cache clear function for home page
+export const clearAnalyticsCache = () => {
+    // This function is called after CSV imports to clear any cached data
+    // Currently, the home page doesn't have client-side caching, but this
+    // function is kept for consistency and future use
+    console.log("[Home] Cache cleared");
+};
