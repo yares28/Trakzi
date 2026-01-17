e985135d57681cdb304dd47ad027b0b2160de434
Your Name <yayafaresPRO@gmail.com>
2026-01-17 18:03:48 +0100

    Score fixes

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -4,11 +4,16 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-function Table({ className, ...props }: React.ComponentProps<"table">) {
+interface TableProps extends React.ComponentProps<"table"> {
+  /** When true, removes the overflow wrapper to allow sticky headers in parent scroll containers */
+  containerClassName?: string
+}
+
+function Table({ className, containerClassName, ...props }: TableProps) {
   return (
     <div
       data-slot="table-container"
-      className="relative w-full overflow-x-auto"
+      className={cn("relative w-full overflow-x-auto custom-scrollbar", containerClassName)}
     >
       <table
         data-slot="table"
